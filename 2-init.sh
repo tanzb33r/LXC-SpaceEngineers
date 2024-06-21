065f492465e6275f4b2399f1e455631a1ff48fe7
Daniel Beer <dbeer@MBP-von-Daniel.fritz.box>
2024-06-21 19:17:57 +0200

    Fix folder paths

--- 2-init.sh
+++ 2-init.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 mkdir -p /appdata/space-engineers/bin
 mkdir -p /appdata/space-engineers/config
-mkdir -p /appdata/space-engineers/bins/SpaceEngineersDedicated
-mkdir -p /appdata/space-engineers/bins/steamcmd
+mkdir -p /appdata/space-engineers/SpaceEngineersDedicated
 mkdir -p /appdata/space-engineers/config/World
 mkdir -p /appdata/space-engineers/config/Plugins
 
@@ -24,16 +23,16 @@ if [ ! -f "/appdata/space-engineers/config/World/Sandbox.sbc" ]; then
 fi
 
 # #check if /appdata/space-engineers/config/SpaceEngineers-Dedicated.cfg is a file
-if [ ! -f "/appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg" ]; then
+if [ ! -f "/appdata/space-engineers/config/SpaceEngineers-Dedicated.cfg" ]; then
   echo "SpaceEngineers-Dedicated.cfg file does not exist, exiting."
   exit 131
 fi
 
 #set <LoadWorld> to the correct value
-cat /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg | sed -E '/.*LoadWorld.*/c\  <LoadWorld>Z:\\appdata\\space-engineers\\World</LoadWorld>' > /tmp/SpaceEngineers-Dedicated.cfg && cat /tmp/SpaceEngineers-Dedicated.cfg > /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg
+cat /appdata/space-engineers/config/SpaceEngineers-Dedicated.cfg | sed -E '/.*LoadWorld.*/c\  <LoadWorld>Z:\\appdata\\space-engineers\\World</LoadWorld>' > /tmp/SpaceEngineers-Dedicated.cfg && cat /tmp/SpaceEngineers-Dedicated.cfg > /appdata/space-engineers/config/SpaceEngineers-Dedicated.cfg
 
 #set game port to the correct value
-#cat /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg | sed -E '/.*ServerPort.*/c\  <ServerPort>27016</ServerPort>' > /tmp/SpaceEngineers-Dedicated.cfg && cat /tmp/SpaceEngineers-Dedicated.cfg > /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg
+#cat /appdata/space-engineers/config/SpaceEngineers-Dedicated.cfg | sed -E '/.*ServerPort.*/c\  <ServerPort>27016</ServerPort>' > /tmp/SpaceEngineers-Dedicated.cfg && cat /tmp/SpaceEngineers-Dedicated.cfg > /appdata/space-engineers/config/SpaceEngineers-Dedicated.cfg
 
 #configure plugins section in SpaceEngineers-Dedicated.cfg
 #get new plugins string
@@ -51,10 +50,12 @@ SED_EXPRESSION_EMPTY="s/<Plugins \/>/${PLUGINS_STRING////\\/} /g"
 SED_EXPRESSION_FULL="s/<Plugins>.*<\/Plugins>/${PLUGINS_STRING////\\/} /g"
 
 #find and replace in SpaceEngineers-Dedicated.cfg for empty "<Plugins />" element
-cat /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg | sed -E "$SED_EXPRESSION_EMPTY" > /tmp/SpaceEngineers-Dedicated.cfg && cat /tmp/SpaceEngineers-Dedicated.cfg > /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg
+cat /appdata/space-engineers/config/SpaceEngineers-Dedicated.cfg | sed -E "$SED_EXPRESSION_EMPTY" > /tmp/SpaceEngineers-Dedicated.cfg && cat /tmp/SpaceEngineers-Dedicated.cfg > /appdata/space-engineers/config/SpaceEngineers-Dedicated.cfg
 
 #find and replace in SpaceEngineers-Dedicated.cfg for filled out "<Plugins>...</Plugins>" element
 # sed can't handle multiple lines easily, so everything needs to be on a single line.
-cat /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg | sed -E "$SED_EXPRESSION_FULL" > /tmp/SpaceEngineers-Dedicated.cfg && cat /tmp/SpaceEngineers-Dedicated.cfg > /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg
+cat /appdata/space-engineers/config/SpaceEngineers-Dedicated.cfg | sed -E "$SED_EXPRESSION_FULL" > /tmp/SpaceEngineers-Dedicated.cfg && cat /tmp/SpaceEngineers-Dedicated.cfg > /appdata/space-engineers/config/SpaceEngineers-Dedicated.cfg
+
+cp /appdata/space-engineers/config/SpaceEngineers-Dedicated.cfg /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg
 
 steamcmd +login anonymous +@sSteamCmdForcePlatformType windows +force_install_dir /appdata/space-engineers/SpaceEngineersDedicated +app_update 298740 +quit
