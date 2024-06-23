b039715f1a531c8d6bd6f3a08a4699a5e330cea0
Daniel Beer <dbeer@MBP-von-Daniel.fritz.box>
2024-06-23 14:54:37 +0200

    Upload new world file

--- 4-start.sh
+++ 4-start.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-cd /appdata/space-engineers/SpaceEngineersDedicated/DedicatedServer64/ && \
 env WINEARCH=win64 WINEDEBUG=-all WINEPREFIX=/wineprefix wine /appdata/space-engineers/SpaceEngineersDedicated/DedicatedServer64/SpaceEngineersDedicated.exe -noconsole -path Z:\\appdata\\space-engineers\\SpaceEngineersDedicated -ignorelastsession
